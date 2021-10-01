50a2363fc21181c03ca24d4e5ea593bd7455f77a
Mostafa Nematpour <nematpor46@gmail.com>
2021-10-01 13:57:27 +0330

    go-to-user-location-finish

--- tpl/tpl_index.php
+++ tpl/tpl_index.php
@@ -20,6 +20,7 @@
         <div class="mapContainer">
             <div id="map"></div>
         </div>
+        <img src="assets/img/current.png" class="currentLoc">
     </div>
 
     <div class="modal-overlay" style="display: none;">
@@ -70,6 +71,11 @@
             L.marker([<?= $location->lat ?>, <?= $location->lng ?>]).addTo(map).bindPopup("<?= $location->title ?>").openPopup();
 
         <?php endif; ?>
+        $(document).ready(function() {
+            $('img.currentLoc').click(function() {
+                locate();
+            });
+        });
     </script>
 
 </body>
